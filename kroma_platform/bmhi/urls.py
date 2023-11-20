e81a18d20fac264b40cf3909ff3d8f3bb309b21c
Dipak Prasad Upadhyaya <70591211+Upadhyaya6@users.noreply.github.com>
2023-11-20 11:20:06 -0500

    removed ORMIS

--- kroma_platform/bmhi/urls.py
+++ kroma_platform/bmhi/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('labwebsite.urls', namespace='labwebsite')),
-    path('ORMIS/', include('ORMIS_PD.urls', namespace='ORMIS')),
     path('NIC/', include('NIC.urls', namespace='NIC')),
     path('NIC/MATILDA/', include('MATILDA.urls', namespace='MATILDA'))
 ]
