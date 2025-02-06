a9712811df928b3b4f1063a6abd4a88332e35e85
Katrina Prantzalos <kdp49@case.edu>
2025-02-05 20:33:53 -0500

    put conditional for DEBUG back for security reasons

--- kroma_platform/bmhi/settings.py
+++ kroma_platform/bmhi/settings.py
@@ -29,12 +29,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#if environment == 'prod':
-#    DEBUG = False
-#else:
-#    DEBUG = True
+if environment == 'prod':
+   DEBUG = False
+else:
+   DEBUG = True
 
-DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
